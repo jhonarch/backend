dc05026813d8f42a045d7438654577366e0a9ccc
jhonarch <jhonarch@gmail.com>
2018-11-17 19:24:36 -0500

    first commit

---
+++ src/test/java/com/arch/notas/NotasApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arch.notas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotasApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
